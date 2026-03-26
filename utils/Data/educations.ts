56cebf72cc2d4067d3e55cb14b937255fe87fde9
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2026-03-26 14:13:32 +0000

    added utils/Data/educations.ts

--- utils/Data/educations.ts
+++ utils/Data/educations.ts
@@ -1,8 +1,21 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor's in Information Technology'",
-    duration: "2020 - Present",
-    institution: "University of Chakwal",
+    title:
+      "Bachelor of Technology - Computer Science and Engineering; CGPA: 6.99",
+    duration: "Since August 2023",
+    institution: "Lovely Professional University, Phagwara, Punjab",
+  },
+  {
+    id: 2,
+    title: "Intermediate; Percentage: 92",
+    duration: "April 2021 - March 2023",
+    institution: "Durga Higher Secondary School, Kanhangad, Kerala",
+  },
+  {
+    id: 3,
+    title: "Matriculation; Percentage: 90",
+    duration: "April 2020 - March 2021",
+    institution: "Sadguru Public School, Kanhangad, Kerala",
   },
 ];
